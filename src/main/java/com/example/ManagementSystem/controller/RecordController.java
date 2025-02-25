99ce9dd3a00104fb7aeb2cf466e75a9f24454831
Dusenn <dusenmaharr@gmail.com>
2025-02-25 21:28:41 +0800

    comment

--- src/main/java/com/example/ManagementSystem/controller/RecordController.java
+++ src/main/java/com/example/ManagementSystem/controller/RecordController.java
@@ -1,30 +1,39 @@
 package com.example.ManagementSystem.controller;
 
 import com.example.ManagementSystem.model.Records;
-import com.example.ManagementSystem.repository.RecordRepository;
 import com.example.ManagementSystem.service.RecordService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @CrossOrigin
-//@RequestMapping("/api/user")
 public class RecordController {
 
-    @Autowired
     private final RecordService recordService;
 
     public RecordController(RecordService recordService) {
         this.recordService = recordService;
     }
 
-//    @GetMapping("/{unitNo}")
-//    public Optional<Records> getRecordById(@PathVariable String unitNo){
-//        return recordService.getRecordByUnitNo(unitNo);
-//    }
+    @GetMapping("/record")
+    public ResponseEntity<Optional<Records>> getRecord(@RequestParam String unitNo) {
+        Optional<Records> record = recordService.getRecordByUnitNo(unitNo);
+        return record.isPresent() ? ResponseEntity.ok(record) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/getAllRecord")
+    public List<Records> getAllRecord() {
+       return recordService.getAllRecords();
+
+        }
+
+    @GetMapping("/countrecord")
+    public String getCountRecord(@RequestBody Records record) {
+        return recordService.getCountRecord(record);
+    }
 
     @GetMapping("/load")
     public String loadData() {
@@ -32,19 +41,14 @@ public class RecordController {
         return "success";
     }
 
+    @PostMapping("/record")
+    public Records addRecord(@RequestBody Records record) {
+        return recordService.addRecord(record);
+    }
+
     @DeleteMapping("/deleterecord")
     public ResponseEntity<String> deleteRecord(@RequestParam String unitNo, @RequestParam String icNo) {
         String result = recordService.deleteRecord(unitNo, icNo);
-
-        if ("Record Not Found".equals(result)) {
-            return ResponseEntity.badRequest().body(result);
-        } else {
-            return ResponseEntity.ok().body(result);
-        }
+        return "Record Not Found".equals(result) ? ResponseEntity.badRequest().body(result) : ResponseEntity.ok(result);
     }
-///test
-    @GetMapping("get")
-    public void test() {
-        
-    }
-    }
+}
